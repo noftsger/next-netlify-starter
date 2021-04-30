971fa4f4311241af838fe2624cca05afcc2d2ff0
noftsger <83439054+noftsger@users.noreply.github.com>
2021-04-29 22:13:29 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          defr
+          defrost
         </p>
       </main>
 
